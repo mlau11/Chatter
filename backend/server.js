c3c667f05eec41f79f5c116d4f3cefa712025bf3
Michael Lau <mlau448@gmail.com>
2022-06-30 04:27:38 -0400

    changed cors origin

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-app-ml.herokuapp.com",
   },
 });
 
